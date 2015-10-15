fd3394159c8d9a00586f3a05e96acdaee2ce757d
Carlos Augusto Daniel da Costa Rubim Leão <thefordexter@gmail.com>
2015-10-15 12:18:07 -0300

    Correção do app e integração com Facebook.
    Adicionada a integração com o Facebook.

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1531695993787452',
+    secret: '4ca7a19a544b4127b8c5a4f2a10c1d3c'
+});
