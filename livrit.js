debb6ec2f19505e3c9aeeb838b96957c8b8de473
Carlos Augusto Daniel da Costa Rubim Leao - DATAPREVRJ <carlos.leao@dataprev.gov.br>
2015-10-27 14:49:05 -0200

    google places autocomplete

--- livrit.js
+++ livrit.js
@@ -1,7 +1,10 @@
 if (Meteor.isClient) {
 
   Meteor.startup(function() {
-    GoogleMaps.load({key: Meteor.settings.public.googleAPIKey});
+    GoogleMaps.load({
+      key: Meteor.settings.public.googleAPIKey,
+      libraries: 'places'
+    });
   });
 
   Router.onBeforeAction(function() {
@@ -78,6 +81,7 @@ if (Meteor.isClient) {
 
     GoogleMaps.ready('mainMap', function(map) {
 
+      // plotar polyline
       var routeSegment = [
         {lat: -22.896078, lng:-43.181556},
         {lat: -22.897288, lng:-43.182078}
@@ -91,7 +95,60 @@ if (Meteor.isClient) {
         strokeWeight: 2,
         map: map.instance
       })
+
+      // autocomplete
+      var input = document.getElementById('search-input');
+
+      var autocomplete = new google.maps.places.Autocomplete(input);
+      autocomplete.bindTo('bounds', map.instance);
+
+      var infowindow = new google.maps.InfoWindow();
+      var marker = new google.maps.Marker({
+        map: map.instance,
+        anchorPoint: new google.maps.Point(0, -29)
+      });
+
+      autocomplete.addListener('place_changed', function() {
+        infowindow.close();
+        marker.setVisible(false);
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+          window.alert("Autocomplete's returned place contains no geometry");
+          return;
+        }
+
+        // If the place has a geometry, then present it on a map.
+        if (place.geometry.viewport) {
+          map.instance.fitBounds(place.geometry.viewport);
+        } else {
+          map.instance.setCenter(place.geometry.location);
+          map.instance.setZoom(17);  // Why 17? Because it looks good.
+        }
+        marker.setIcon(/** @type {google.maps.Icon} */({
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(35, 35)
+        }));
+        marker.setPosition(place.geometry.location);
+        marker.setVisible(true);
+
+        var address = '';
+        if (place.address_components) {
+          address = [
+            (place.address_components[0] && place.address_components[0].short_name || ''),
+            (place.address_components[1] && place.address_components[1].short_name || ''),
+            (place.address_components[2] && place.address_components[2].short_name || '')
+          ].join(' ');
+        }
+
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+        infowindow.open(map.instance, marker);
+      });
+
     });
+
   });
 }
 
